93c6db87551a763aaaf6865d59b6d286aa42e911
omutwar <omutwar@hotmail.com>
2024-05-05 09:04:59 -0500

    screenshot updated

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -11,23 +11,20 @@ import io.cucumber.java.Scenario;
 import utils.Driver;
 
 public class Hooks {
-	
+
 	@Before
 	public void setUp() {
 		Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		//Driver.getDriver().manage().window().maximize();
+		// Driver.getDriver().manage().window().maximize();
 	}
-	
+
 	@After
 	public void tearDown(Scenario scenario) {
 		if (scenario.isFailed()) {
-			final byte[] screenshot = ((TakesScreenshot) 
-					Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
-			scenario.attach(screenshot, "image/png", "screenshot");
+			final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(screenshot, "image/png", "./reports/screenshot/screenshot");
 		}
 		Driver.quitDriver();
 	}
-	
-	
-	}
-	
+
+}
